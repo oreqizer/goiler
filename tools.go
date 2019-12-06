be749e743d0b2fa489fe083b4391675c58233d96
oreqizer <oreqizer@gmail.com>
2019-12-06 11:31:13 +0100

    docs: tools

--- tools.go
+++ tools.go
@@ -6,3 +6,7 @@ import (
 	// go run github.com/vektah/dataloaden AccountLoader string '*github.com/oreqizer/goiler/graphql/schemas/account.Account'
 	_ "github.com/vektah/dataloaden"
 )
+
+// Other generators:
+
+// go run cmd/schemagen/main.go -struct Account -models 'github.com/oreqizer/goiler/models'
